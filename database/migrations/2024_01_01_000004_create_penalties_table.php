6e2d13eaa96400b15a308a17db522b249b759191
lusse-ops <marvinwilliamlusse@gmail.com>
2026-02-16 19:55:00 +0300

    Create migration file 2024_01_01_000004_create_penalties_table.php

---
+++ database/migrations/2024_01_01_000004_create_penalties_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+
+class CreatePenaltiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penalties', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->decimal('amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penalties');
+    }
+}
